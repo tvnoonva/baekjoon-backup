a7f3b69ef7213a7d6b004f48b141234b19685cb3
tvnoonva <149685968+tvnoonva@users.noreply.github.com>
2023-11-03 00:00:26 +0900

    Add files via upload

---
+++ num10430.c
@@ -0,0 +1,18 @@
+/*
+
+#include <stdio.h>
+
+int main(void) {
+	int numA, numB, numC;
+	int Ac, Bc;
+	scanf("%d %d %d", &numA, &numB, &numC);
+	Ac = numA % numC;
+	Bc = numB % numC;
+
+	printf("%d\n", (numA + numB) % numC);
+	printf("%d\n", (Ac + Bc) % numC);
+	printf("%d\n", (numA * numB) % numC);
+	printf("%d\n", (Ac * Bc) % numC);
+}
+
+*/
